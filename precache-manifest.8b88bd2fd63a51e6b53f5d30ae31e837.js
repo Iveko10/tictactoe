62353dbb23462e9c7da8acaaf6571eb9a2bfebb9
Ivan Sefcik <isefcik@centrum.sk>
2019-10-14 19:06:52 +0200

    Updates

---
+++ precache-manifest.8b88bd2fd63a51e6b53f5d30ae31e837.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c1917cd2f39fb4a4a7b484eb45d6f3",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "1e22cb1392bf46ef4992",
+    "url": "/tictactoe/static/css/main.ed3a4c85.chunk.css"
+  },
+  {
+    "revision": "d4b1d08f33a52492ca0e",
+    "url": "/tictactoe/static/js/2.dc9fdf45.chunk.js"
+  },
+  {
+    "revision": "1e22cb1392bf46ef4992",
+    "url": "/tictactoe/static/js/main.db5a3e90.chunk.js"
+  },
+  {
+    "revision": "3546a54929d4705c9559",
+    "url": "/tictactoe/static/js/runtime-main.14c4d284.js"
+  }
+]);
